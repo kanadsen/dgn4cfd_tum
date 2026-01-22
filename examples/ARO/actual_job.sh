c750d7312beb811056218bb30962d7660c6d46f9
KANAD SEN <65330799+kanadsen@users.noreply.github.com>
2026-01-22 02:17:18 -0500

    Update

--- examples/ARO/actual_job.sh
+++ examples/ARO/actual_job.sh
@@ -4,7 +4,11 @@
 #PBS -l filesystems=grand:home
 #PBS -A NeuralDE
 
+set -x
+set -e
+
 PROJECT_DIR=/lus/grand/projects/NeuralDE/kanadsen/myrepos/dgn4cfd_tum/examples/ARO
+ls -ld $PROJECT_DIR
 cd $PROJECT_DIR || exit 1
 
 source /lus/grand/projects/NeuralDE/kanadsen/environment_folders/dgn_new_env/bin/activate
